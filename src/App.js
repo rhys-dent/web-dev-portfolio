17fd9da1a2c1f3c050d40cd0b9c1b7c53a8da269
Rhys Dent <devRhysDent@gmail.com>
2021-07-04 20:48:19 -0600

    project data is requested from github instead of hard-coded

--- src/App.js
+++ src/App.js
@@ -2,173 +2,179 @@ import { useEffect, useRef, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import { Header, Resume, Contact, Footer } from "./";
-import { projects } from "./data";
+
 import ScrollBar from "./ScrollBar";
 
 const OuterS = styled.div`
-  box-sizing: border-box;
-  height: 100vh;
-  width: 100vw;
-  overflow-y: hidden;
-  overflow-x: hidden;
+	box-sizing: border-box;
+	height: 100vh;
+	width: 100vw;
+	overflow-y: hidden;
+	overflow-x: hidden;
 `;
 
 const HeaderS = styled.div`
-  position: absolute;
-  width: 100%;
+	position: absolute;
+	width: 100%;
 `;
 
 const ScrollerS = styled.div`
-  --header-height: 10vh;
-  padding-top: 15vh;
-  box-sizing: border-box;
-  border: 0vh solid fuchsia;
-  height: 100vh;
-  width: 100vw;
-  overflow-y: scroll;
-  background: linear-gradient(to right, black 30%, white 100%);
+	--header-height: 10vh;
+	padding-top: 15vh;
+	box-sizing: border-box;
+	border: 0vh solid fuchsia;
+	height: 100vh;
+	width: 100vw;
+	overflow-y: scroll;
+	background: linear-gradient(to right, black 30%, white 100%);
 `;
 
 const IntroS = styled.div`
-  width: 75%;
-  margin: 2rem auto;
-  .intro-inner {
-    display: flex;
-    background-color: rgba(0, 0, 0, 0.125);
-    justify-content: space-evenly;
-    align-items: center;
-    border-radius: 1vh;
-    box-shadow: 0 0 12px white;
-    padding: 4rem;
-    .bio {
-      font-size: 1.5rem;
-      color: whitesmoke;
-      text-shadow: 0px 0px 8px black;
-    }
-  }
+	width: 75%;
+	margin: 2rem auto;
+	.intro-inner {
+		display: flex;
+		background-color: rgba(0, 0, 0, 0.125);
+		justify-content: space-evenly;
+		align-items: center;
+		border-radius: 1vh;
+		box-shadow: 0 0 12px white;
+		padding: 4rem;
+		.bio {
+			font-size: 1.5rem;
+			color: whitesmoke;
+			text-shadow: 0px 0px 8px black;
+		}
+	}
 `;
 
 const ProjectS = styled.div`
-  box-sizing: border-box;
-  position: relative;
-  z-index: 0;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  
-  color: whitesmoke;
-  
-  box-shadow: 0 0 8px black;
-  border: 1px solid transparent;
-  
-  margin: 2rem 4rem;
-  background-color: black;
-  
-  border-radius: 4px;
-  @media only screen and (max-width: 600px) {
-    display:block;
-  }
-  img {
-    width:100%;
-    
-    box-shadow: 0 0 12px whitesmoke;
-    outline: none;
-    border: none;
-  }
-  div {
-    padding: 1rem;
-    p {
-      font-size: 1.5rem;
-    }
-    ul {
-    }
-  }
-  .link-hover-message {
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(255, 255, 255, 0.25);
-    border: 1vh solid rgba(255, 255, 255, 0.25);
-    border-radius: 4px;
-    width: 100%;
-    height: 100%;
-    h2 {
-      font-size: 2rem;
-      padding: 2rem;
-      border-radius: 4px;
-      background-color: rgba(255, 255, 255, 0.9);
-      color: black;
-    }
-  }
-  :hover {
-    .link-hover-message {
-      opacity: 1;
-    }
-  }
+	box-sizing: border-box;
+	position: relative;
+	z-index: 0;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+
+	color: whitesmoke;
+
+	box-shadow: 0 0 8px black;
+	border: 1px solid transparent;
+
+	margin: 2rem 4rem;
+	background-color: black;
+
+	border-radius: 4px;
+	@media only screen and (max-width: 600px) {
+		display: block;
+	}
+	img {
+		width: 100%;
+
+		box-shadow: 0 0 12px whitesmoke;
+		outline: none;
+		border: none;
+	}
+	div {
+		padding: 1rem;
+		p {
+			font-size: 1.5rem;
+		}
+		ul {
+		}
+	}
+	.link-hover-message {
+		opacity: 0;
+		position: absolute;
+		top: 0;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: rgba(255, 255, 255, 0.25);
+		border: 1vh solid rgba(255, 255, 255, 0.25);
+		border-radius: 4px;
+		width: 100%;
+		height: 100%;
+		h2 {
+			font-size: 2rem;
+			padding: 2rem;
+			border-radius: 4px;
+			background-color: rgba(255, 255, 255, 0.9);
+			color: black;
+		}
+	}
+	:hover {
+		.link-hover-message {
+			opacity: 1;
+		}
+	}
 `;
 
 function App() {
-  const header = useRef();
-  const scroller = useRef();
+	const header = useRef();
+	const scroller = useRef();
+	const [projects, setProjects] = useState([]);
+	const projectsDataUrl =
+		"https://raw.githubusercontent.com/rhys-dent/web-dev-portfolio-projects/main/";
+	useEffect(function () {
+		fetch(projectsDataUrl + "data.json")
+			.then((response) => response.json())
+			.then((data) => setProjects(data));
+	}, []);
+	const [scrollPercent, setScrollPercent] = useState();
+	const [scrollLimit, setScrollLimit] = useState(0);
 
-  const [scrollPercent, setScrollPercent] = useState();
-  const [scrollLimit, setScrollLimit] = useState(0);
+	let headerHeightPx;
 
-  let headerHeightPx;
+	function onScroll(e) {
+		const scrollTop = Math.round(scroller.current.scrollTop);
+		setScrollPercent(scrollTop / scrollLimit);
 
-  function onScroll(e) {
-    const scrollTop = Math.round(scroller.current.scrollTop);
-    setScrollPercent(scrollTop / scrollLimit);
-
-    scroller.current.style.backgroundPosition = `100% ${(1 - scrollPercent) * 100
-      }%`;
-  }
-  return (
-    <OuterS>
-      <Router>
-        <HeaderS ref={header}>
-          <Header />
-        </HeaderS>
-        <ScrollerS ref={scroller} onScroll={onScroll}>
-
-          <Switch>
-            <Route exact path="/">
-
-              {projects.map((project) => (
-                <ProjectS>
-                  <a className="link-hover-message" href={project.url}>
-                    <h2>Visit Site</h2>
-                  </a>
-                  <div>
-                    <img src={project.src} alt="" />
-                  </div>
-                  <div>
-                    <p className="context-text">{project.description}</p>
-                    <h3>Created Using:</h3>
-                    <ul>
-                      {project.technologies.map((technology) => (
-                        <li>{technology}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </ProjectS>
-              ))}
-            </Route>
-            <Route path="/resume">
-              <Resume />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-          </Switch>
-          <Footer />
-        </ScrollerS>
-      </Router>
-    </OuterS>
-  );
+		scroller.current.style.backgroundPosition = `100% ${
+			(1 - scrollPercent) * 100
+		}%`;
+	}
+	return (
+		<OuterS>
+			<Router>
+				<HeaderS ref={header}>
+					<Header />
+				</HeaderS>
+				<ScrollerS ref={scroller} onScroll={onScroll}>
+					<Switch>
+						<Route exact path="/">
+							{projects.map((project) => (
+								<ProjectS>
+									<a className="link-hover-message" href={project.url}>
+										<h2>Visit Site</h2>
+									</a>
+									<div>
+										<img src={projectsDataUrl + project.src} alt="" />
+									</div>
+									<div>
+										<p className="context-text">{project.description}</p>
+										<h3>Created Using:</h3>
+										<ul>
+											{project.technologies.map((technology) => (
+												<li>{technology}</li>
+											))}
+										</ul>
+									</div>
+								</ProjectS>
+							))}
+						</Route>
+						<Route path="/resume">
+							<Resume />
+						</Route>
+						<Route path="/contact">
+							<Contact />
+						</Route>
+					</Switch>
+					<Footer />
+				</ScrollerS>
+			</Router>
+		</OuterS>
+	);
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,136 +1,142 @@
+fetch(
+	"https://raw.githubusercontent.com/rhys-dent/web-dev-portfolio-projects/main/data.json"
+)
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+
 export const projects = [
-  {
-    url: "http://prometheuspm.com/",
-    src: "prometheus-property-management.jpg",
-    description:
-      "A brochure website developed during my practicum to help promote my employer's property management to potential clients. ",
-    technologies: ["ReactJS", "Amplify (Amazon Web Service)"],
-  },
-  {
-    url: "https://master.dojb0il0pmvkq.amplifyapp.com/",
-    src: "kanaquiz.jpg",
-    description:
-      "A simple web application intended to help to users memorize the Japanese alphabet. Both hiragana and katana.",
-    technologies: ["ReactJS", "Amplify (Amazon Web Service)"],
-  },
- 
-  {
-    url: "https://tallycraft.rhysdentportfolio.ca/",
-    src: "tallycraft.jpg",
-    description:
-      "A single page website created to share my mother's crafting hobby.",
-    technologies: [
-      "Vanilla Javascript",
-      "Amazon CloudFront",
-      "Amazon S3",
-      "Amazon Certificate Manager"
-    ],
-  },
+	{
+		url: "http://prometheuspm.com/",
+		src: "prometheus-property-management.jpg",
+		description:
+			"A brochure website developed during my practicum to help promote my employer's property management to potential clients. ",
+		technologies: ["ReactJS", "Amplify (Amazon Web Service)"],
+	},
+	{
+		url: "https://master.dojb0il0pmvkq.amplifyapp.com/",
+		src: "kanaquiz.jpg",
+		description:
+			"A simple web application intended to help to users memorize the Japanese alphabet. Both hiragana and katana.",
+		technologies: ["ReactJS", "Amplify (Amazon Web Service)"],
+	},
+
+	{
+		url: "https://tallycraft.rhysdentportfolio.ca/",
+		src: "tallycraft.jpg",
+		description:
+			"A single page website created to share my mother's crafting hobby.",
+		technologies: [
+			"Vanilla Javascript",
+			"Amazon CloudFront",
+			"Amazon S3",
+			"Amazon Certificate Manager",
+		],
+	},
 ];
 
 export const resume = {
-  skills: {
-    major: [
-      "HTML5",
-      "CSS3",
-      "Javascript",
-      "ReactJS",
-      "NoSQL",
-      "Amazon Web Services",
-      //"Effective Communicator and Team Worker",
-    ],
-    minor: [],
-  },
-  education: [
-    {
-      title: "Web and Application Development",
-      institution: "CDI College",
-      start: "October 2019",
-      completed: "March 2020",
-    },
-    {
-      title: "High School Diploma",
-      institution: "Henry Wise Wood High School",
-      start: "Septemeber 2004",
-      completed: "June 2007",
-    },
-  ],
-  experience: [
-    {
-      title: "Web Developer",
-      company: "Prometheus Poperty Management",
-      location: "Calgary, AB",
-      start: "February 2021",
-      end: "March 2021",
-      description: [
-        "Developed a responsive ReactJS website to help promote the business to property owners ",
-        "Loaded and unloaded approximately 40 scooters per trip and completed 2 - 3 trips per night. Each scooter is either 30 lbs or 45 lbs, depending on the model",
-        "Worked long and late hours. Starting around 9:00 PM and finishing around 7:00 AM",
-        "Was responsible for my own schedule and hours",
-      ],
-    },
-    {
-      title: 'Lime Scooter "Juicer"',
-      company: "Neutron Holdings, Inc",
-      location: "Calgary, AB",
-      start: "August 2019",
-      end: "Present",
-      description: [
-        "Collected electric scooters from the downtown area in order to personally charge at my own home",
-        "Developed am efficient routine that allowed me to collect more scooters while the previous batch was charging",
-        "Loaded and unloaded approximately 40 scooters per trip and completed 2 - 3 trips per night. Each scooter is either 30 lbs or 45 lbs, depending on the model",
-        "Worked long and late hours. Starting around 9:00 PM and finishing around 7:00 AM",
-        "Was entirely responsible for my own schedule and hours",
-      ],
-    },
-    {
-      title: "File Clerk and Moving Crew Member",
-      company: "Advantage Oil and Gas",
-      location: "Calgary, AB",
-      start: "March 2019",
-      end: "July 2019",
-      description: [
-        "Reorganized several important well filing systems",
-        "Moved furniture, equipment, etc. for an office of approximately 40 people from the 3rd to 22nd floor",
-      ],
-    },
-    {
-      title: "Line Cook",
-      company: "Richmonds Pub and Eatery",
-      location: "Calgary, AB",
-      start: "May 2012",
-      end: "December 2018",
-      description: [
-        "Employed for periods of either part time or on-call work",
-        "Had to quickly adapt to new menu items and working with new team members",
-        "Offered general support to all parts of the kitchen",
-      ],
-    },
-    {
-      title: "Expediter",
-      company: "Earl’s West Hills",
-      location: "Calgary, AB",
-      start: "September 2015",
-      end: "December 2016",
-      description: [
-        "Helped manage employees during peak hours",
-        "Was responsible for all menu items immediately after the leaving the kitchen",
-        "This included quality checking each item as well as coordinating cooks and servers in a manner that would ensure the customer received their meal promptly after cooking",
-      ],
-    },
-    {
-      title: "Line Cook and Expediter",
-      company: "Earl’s Willow Park",
-      location: "Calgary, AB",
-      start: "Janurary 2009",
-      end: "June 2013",
-      description: [
-        "Kept up to date with seasonal menu changes and entirely new roll outs",
-        "Managed a unit of cooks (typically four) as part of a larger team",
-        "Was responsible for appropriate food stock levels on line, in accordance with projected sales",
-        "Often served around $10,000 worth of food in sales during dinner hours",
-        "Was responsible for cleaning and sanitizing the station and equipment at the end of the shift",
-      ],
-    },
-  ],
+	skills: {
+		major: [
+			"HTML5",
+			"CSS3",
+			"Javascript",
+			"ReactJS",
+			"NoSQL",
+			"Amazon Web Services",
+			//"Effective Communicator and Team Worker",
+		],
+		minor: [],
+	},
+	education: [
+		{
+			title: "Web and Application Development",
+			institution: "CDI College",
+			start: "October 2019",
+			completed: "March 2020",
+		},
+		{
+			title: "High School Diploma",
+			institution: "Henry Wise Wood High School",
+			start: "Septemeber 2004",
+			completed: "June 2007",
+		},
+	],
+	experience: [
+		{
+			title: "Web Developer",
+			company: "Prometheus Poperty Management",
+			location: "Calgary, AB",
+			start: "February 2021",
+			end: "March 2021",
+			description: [
+				"Developed a responsive ReactJS website to help promote the business to property owners ",
+				"Loaded and unloaded approximately 40 scooters per trip and completed 2 - 3 trips per night. Each scooter is either 30 lbs or 45 lbs, depending on the model",
+				"Worked long and late hours. Starting around 9:00 PM and finishing around 7:00 AM",
+				"Was responsible for my own schedule and hours",
+			],
+		},
+		{
+			title: 'Lime Scooter "Juicer"',
+			company: "Neutron Holdings, Inc",
+			location: "Calgary, AB",
+			start: "August 2019",
+			end: "Present",
+			description: [
+				"Collected electric scooters from the downtown area in order to personally charge at my own home",
+				"Developed am efficient routine that allowed me to collect more scooters while the previous batch was charging",
+				"Loaded and unloaded approximately 40 scooters per trip and completed 2 - 3 trips per night. Each scooter is either 30 lbs or 45 lbs, depending on the model",
+				"Worked long and late hours. Starting around 9:00 PM and finishing around 7:00 AM",
+				"Was entirely responsible for my own schedule and hours",
+			],
+		},
+		{
+			title: "File Clerk and Moving Crew Member",
+			company: "Advantage Oil and Gas",
+			location: "Calgary, AB",
+			start: "March 2019",
+			end: "July 2019",
+			description: [
+				"Reorganized several important well filing systems",
+				"Moved furniture, equipment, etc. for an office of approximately 40 people from the 3rd to 22nd floor",
+			],
+		},
+		{
+			title: "Line Cook",
+			company: "Richmonds Pub and Eatery",
+			location: "Calgary, AB",
+			start: "May 2012",
+			end: "December 2018",
+			description: [
+				"Employed for periods of either part time or on-call work",
+				"Had to quickly adapt to new menu items and working with new team members",
+				"Offered general support to all parts of the kitchen",
+			],
+		},
+		{
+			title: "Expediter",
+			company: "Earl’s West Hills",
+			location: "Calgary, AB",
+			start: "September 2015",
+			end: "December 2016",
+			description: [
+				"Helped manage employees during peak hours",
+				"Was responsible for all menu items immediately after the leaving the kitchen",
+				"This included quality checking each item as well as coordinating cooks and servers in a manner that would ensure the customer received their meal promptly after cooking",
+			],
+		},
+		{
+			title: "Line Cook and Expediter",
+			company: "Earl’s Willow Park",
+			location: "Calgary, AB",
+			start: "Janurary 2009",
+			end: "June 2013",
+			description: [
+				"Kept up to date with seasonal menu changes and entirely new roll outs",
+				"Managed a unit of cooks (typically four) as part of a larger team",
+				"Was responsible for appropriate food stock levels on line, in accordance with projected sales",
+				"Often served around $10,000 worth of food in sales during dinner hours",
+				"Was responsible for cleaning and sanitizing the station and equipment at the end of the shift",
+			],
+		},
+	],
 };
