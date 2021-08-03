c47417679afc1d2513ba86fe9b3101ea6562ed93
Rhys Dent <devRhysDent@gmail.com>
2021-08-03 10:59:53 -0600

    updated about section

--- src/About.js
+++ src/About.js
@@ -32,28 +32,26 @@ export default function () {
 
 			<p> Hello! Thank you for visiting my portfolio.</p>
 			<p>
-				My name is Rhys and I am a web developer with a focus on ReactJS and
-				Amazon Web Services. About 4 years ago I discovered a passion for coding
-				and have been pursuing this passion in my free time across a variety of
-				different languages and platforms, initially focusing on game
-				development using Unity3D and mobile app development with Android
-				Studio. This eventually led to me enrolling at CDI College and
-				graduating with a diploma in Web and Application Development. During my
-				practicum I developed a react web app for a property management
-				business. My employer was pleasantly surprised by the results which left
-				me very excited by the prospect of assisting more businesses with their
-				web development needs.
+				My name is Rhys Dent and I am a web developer with a focus on ReactJS
+				and Amazon Web Services. About 4 years ago I discovered a passion for
+				coding and pursued it as a hobby across a variety of different languages
+				and platforms, initially focusing on game development using Unity3D and
+				mobile app development with Android Studio. This eventually led to me
+				enrolling at CDI College and graduating with a diploma in Web and
+				Application Development. During my practicum I developed a React web app
+				for a property management business. My employer was very satisfied with
+				the results, this has left me eager to provide my abilities to other
+				businesses and individuals with web development needs.
 			</p>
 			<p>
 				My work experience outside of web development has given me a broad set
 				of skills. Most notably, I have worked construction outside all year
-				round as a subcontractor and as a line-cook/supervisor at a very busy
-				and fast paced restaurant. These experiences have taught me how to
-				maintain the right attitude, stay focused, stay productive, and be an
-				effective team member under stressful conditions. Currently my free time
-				is dedicated to street performing. Singing in public has expanded my
-				comfort zone and has helped tremendously with improving my communication
-				abilities.
+				round as a subcontractor and as a line-cook/supervisor at very busy and
+				fast-paced restaurants. These experiences have taught me how to maintain
+				the right attitude, stay focused, and stay productive under stressful
+				conditions. Currently my free time is dedicated to expanding my comfort
+				zone by singing and playing guitar around the city as a street
+				performer.
 			</p>
 		</AboutS>
 	);
